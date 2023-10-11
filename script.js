feeec57538300c158fa170693552360a6f1d7bd7
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2023-10-11 22:35:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // your code here
+const nameInput = document.getElementById('name');
+    const yearInput = document.getElementById('year');
+    const urlText = document.getElementById('url');
+    const generateButton = document.getElementById('button');
+    
+    generateButton.addEventListener('click', function() {
+      const name = nameInput.value;
+      const year = yearInput.value;
+      const queryString = `?name=${name}&year=${year}`;
+      urlText.textContent = `https://localhost:8080/${queryString}`;
+    });
